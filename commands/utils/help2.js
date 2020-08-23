02a9f59b330572f065f806f992e11a838d22060c
3DNinja54 <68089561+3DNinja54@users.noreply.github.com>
2020-08-22 21:25:43 -0400

    ⚗️Experimental!

--- commands/utils/help2.js
+++ commands/utils/help2.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const config = require('../,./config.json');
+const config = require('../../config.json');
 const prefixes = config.prefixes;
 
 module.exports = {
