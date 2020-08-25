d908f13ceff660c8dc43f6f7415342e48b223cf1
3DNinja54 <68089561+3DNinja54@users.noreply.github.com>
2020-08-25 01:21:13 -0400

    ✨Beta of 2.1

---
+++ rpg/balance.js
@@ -0,0 +1,24 @@
+module.exports = {
+    name: 'balance',
+    aliases: ['bal'],
+    permissions: [],
+    ownerOnly: false,
+    enabled: true,
+    cooldown: 5,
+    exec: async (client, message, args) => {
+        const Discord = require("discord.js");
+
+        const userData = await client.models.userfindById(message.author.id)
+        if(!userData) await client.models.user.create({ _id: message.author.id })
+
+        let money = userData.money
+
+        const embed = new Discord.MessageEmbed()
+        .addTitle(`${message.author.username}'s Balance`)
+        .addFields(
+            {name: 'Balance', value: `${money}`, inline: true}
+)
+        .addFooter("CURRENTLY IN BETA")
+        message.chanel.send(embed);
+    }
+}
