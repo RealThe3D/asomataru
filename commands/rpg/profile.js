d018179dadf156af716fa11aea29e119d9b13f01
3DNinja54 <68089561+3DNinja54@users.noreply.github.com>
2020-08-30 18:29:12 -0400

    ✨Fixed in v2.1!

---
+++ commands/rpg/profile.js
@@ -0,0 +1,25 @@
+module.exports = {
+    name: 'profile',
+    aliases: ['stats'],
+    permissions: [],
+    ownerOnly: false,
+    enabled: true,
+    cooldown: 0,
+    exec: async (client, message, args) => {
+      const User = require('../../models/userModel.js');
+        const Discord = require('discord.js');
+      let member = message.guild.member(message.mentions.users.first() || message.author)
+      if(member.user.bot) return message.reply(`That is a bot.`)
+      let data = await User.findOne({userID: member.user.id })
+  
+      if(!data) await User.create({ userID: member.user.id })
+  
+      const embed = new Discord.MessageEmbed()
+      .setTitle(`${message.author.username}'s Balance`)
+      .setFields(
+        {name: 'Coins', value: `${data.cash} coins!`}, // add more in the future.
+)
+      .setFooter('Asomataru RPG System v0.1 Beta!')
+      message.channel.send(embed)
+    }
+  }
--- commands/rpg/work.js
+++ commands/rpg/work.js
@@ -1,26 +1,28 @@
 module.exports = {
     name: 'work',
-    aliases: [],
+    aliases: ['adventure','adv'],
     permissions: [],
     ownerOnly: false,
     enabled: true,
     cooldown: 60,
     exec: async (client, message, args) => {
+        const User = require('../../models/userModel.js');
         const Discord = require("discord.js");
 
         let randomAmount = Math.floor(Math.random()* Math.floor(100)) // 1-100
 
-        const userData = await client.models.user.findById(message.author.id)
-        if(!userData) await client.models.user.create({ _id: message.author.id })
+        let data = await User.findOne({userID: message.author.id })
+  
+        if(!data) await User.create({ userID: message.author.id })
 
         const embed = new Discord.MessageEmbed()
-        .addTitle(`${message.author.username} traveled and did some bounties`)
-        .addDescription(`${message.author.username} has earned ${randomAmount} credits.`)
-        .addFooter('CURRENTLY IN BETA!')
+        .setTitle(`${message.author.username} traveled and did some bounties!`)
+        .setDescription(`${message.author.username} has earned ${randomAmount} credits.`)
+        .setFooter('Asomataru RPG System v0.1 Beta!')
 
         message.channel.send(embed);
         
-        userData.money = userData.money + randomAmount 
-        userData.save()
+        data.coins = data.coins + randomAmount 
+        data.save()
     }
 }
