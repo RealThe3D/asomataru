ae624aef6b897eb3682979686aee20ae049995f6
3DNinja54 <68089561+3DNinja54@users.noreply.github.com>
2020-08-31 20:44:55 -0400

    ✨Updated to v2.1.1!

--- models/userModel.js
+++ models/userModel.js
@@ -2,15 +2,14 @@ const mongoose = require("mongoose");
 
 
 const UserSchema = new mongoose.Schema ({
-    account: {
-        userID: String,
+    userID: String,
 
     coins: {type: Number, default: 0},
     xp: {type: Number, default: 0},
     level: {type: Number, default: 1},
     xpRemaining: {type: Number, default: 0},
     hp: {type: Number, default: 20},
-    }
+    
 });
 
 module.exports = mongoose.model("User", UserSchema);
