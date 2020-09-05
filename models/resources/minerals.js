eae9f02added5c1565d645435a2ec824208ad060
3DNinja54 <68089561+3DNinja54@users.noreply.github.com>
2020-09-05 19:05:13 -0400

    ✨Update to v2.1.2!

---
+++ models/resources/minerals.js
@@ -0,0 +1,55 @@
+const resources = {
+	// WOOD
+	["oak wood"]: {
+		type: Number,
+		default: 5,
+	},
+
+	// ORE
+	["copper ore"]: {
+		type: Number,
+		default: 10,
+	},
+	["iron ore"]: {
+		type: Number,
+		default: 0,
+	},
+	["mythril ore"]: {
+		type: Number,
+		default: 0,
+	},
+	["platinum ore"]: {
+		type: Number,
+		default: 0,
+	},
+
+	// BARS
+	["copper bar"]: {
+		type: Number,
+		default: 0,
+	},
+	["iron bar"]: {
+		type: Number,
+		default: 0,
+	},
+	["mythril bar"]: {
+		type: Number,
+		default: 0,
+	},
+	["platinum bar"]: {
+		type: Number,
+		default: 0,
+	},
+	["titanium bar"]: {
+		type: Number,
+		default: 0,
+	},
+
+	// MISC
+	["cobblestone"]: {
+		type: Number,
+		default: 0,
+	},
+};
+
+module.exports = { resources };
--- models/userModel.js
+++ models/userModel.js
@@ -1,15 +1,17 @@
 const mongoose = require("mongoose");
 
-
+const { resources } = require('./resources/minerals.js');
+const { fish } = require('./resources/fish.js');
 const UserSchema = new mongoose.Schema ({
     userID: String,
 
     coins: {type: Number, default: 0},
     xp: {type: Number, default: 0},
     level: {type: Number, default: 1},
-    xpRemaining: {type: Number, default: 100},
+    xptoNextLevel: {type: Number, default: 100},
     hp: {type: Number, default: 20},
-
+    resources,
+    fish
 })
 
 module.exports = mongoose.model("User", UserSchema);
