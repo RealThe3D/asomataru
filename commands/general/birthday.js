690c6e42115853e31592290013d621cf2d4b5b22
3DNinja54 <68089561+3DNinja54@users.noreply.github.com>
2020-09-16 20:21:49 -0400

    🎂Pre-Birthday Update | Updated birthday.js!
    Birthday is on September 17th!

--- commands/general/birthday.js
+++ commands/general/birthday.js
@@ -6,6 +6,21 @@ module.exports = {
     enabled: true,
     cooldown: 0,
     exec: async (client, message, args) => {
-        message.channel.send("3D's Birthday is on September 17!")
+        const Discord = require('discord.js');
+        const Eri = ['https://media1.tenor.com/images/f282775bfa181e9251023892a726037b/tenor.gif?itemid=16645535',
+                     'https://media.tenor.com/images/804187a5ae8a39af449d06d353800692/tenor.gif',
+                     'https://media.tenor.com/images/f81582db3a15b82df5a0b44f378b7898/tenor.gif',
+                     'https://media.tenor.com/images/c4297bc2cf2afd60e2defdaa6bf18dcc/tenor.gif',
+                     'https://media.tenor.com/images/ac76b9ffb3548c1d4cac11f0f5c15218/tenor.gif'
+                     
+            ]
+        var EriRandom = Eri[Math.floor(Math.random() * Eri.length)];
+        const embed = new Discord.MessageEmbed()
+        .setColor('FFC0CB')
+        .setTitle('HAPPY BIRTHDAY, The3D!')
+        .setImage(EriRandom)
+        .setFooter(`It is 3D's Birthday today!, DM The3D#1120 a happy birthday!`)
+
+        message.channel.send(embed)
     },
 };
