b3db045a6fa60a59d6d3bf1cd29f07ed52d69056
3DNinja54 <68089561+3DNinja54@users.noreply.github.com>
2020-09-17 00:32:13 -0400

    🎂 Pre-Birthday Update | Updated birthday.js
    Birthday is September 17th which is today for EDT! DM The3D#1120 Happy Birthday on the 17th!

--- commands/general/birthday.js
+++ commands/general/birthday.js
@@ -9,7 +9,7 @@ module.exports = {
         // Public on Septemeber 17th!
         const Discord = require('discord.js');
         const Eri = ['https://media1.tenor.com/images/f282775bfa181e9251023892a726037b/tenor.gif?itemid=16645535',
-                     'https://media.tenor.com/images/804187a5ae8a39af449d06d353800692/tenor.gif',
+                     'https://media1.tenor.com/images/a276be1f5dd04a52672bb930d35c1e28/tenor.gif?itemid=16475061',
                      'https://media.tenor.com/images/f81582db3a15b82df5a0b44f378b7898/tenor.gif',
                      'https://media.tenor.com/images/c4297bc2cf2afd60e2defdaa6bf18dcc/tenor.gif',
                      'https://media.tenor.com/images/ac76b9ffb3548c1d4cac11f0f5c15218/tenor.gif'
