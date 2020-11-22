43cc6c6e2be8d2ae5bac562f23623c7d19a3a4a7
3DNinja54 <68089561+3DNinja54@users.noreply.github.com>
2020-11-22 17:47:53 -0500

    🎄Updated to Asomataru v2.2.1.1 | Hotfix 1

--- commands/rpg/daily.js
+++ commands/rpg/daily.js
@@ -7,13 +7,13 @@ module.exports = {
     cooldown: 86400,
     exec: async (client, message, args) => {
         const User = require('../../models/userModel.js');
+        const Discord = require("discord.js");
         let randomAmount = Math.floor(Math.random()* Math.floor(750)) // 1-750
         let data = await User.findOne({userID: message.author.id })
   
         if(!data) {
             message.channel.send(`You've have not registered yet, please use a!profile`)
 } else {
-        const Discord = ("require.js");
         const embed = new Discord.MessageEmbed()
         .setTitle(`${message.author.username}'s Daily Rewards`)
         .setDescription(`Your daily reward is ${randomAmount} coins!`)
