1bbb5f754ca7e88bfbc9593fd345c5df070193c4
3DNinja54 <68089561+3DNinja54@users.noreply.github.com>
2021-01-03 00:57:50 -0500

    🎉Asomataru v2.2.3 | 2021 Update!

--- commands/owner/eval.js
+++ commands/owner/eval.js
@@ -8,5 +8,16 @@ module.exports = {
     exec: async (client, message, args) => {
         const code = args.join(" ");
         return evalCmd(message, code);
+
+        function evalCmd(message, code) {
+            try {
+                let evaled = eval(code);
+                if (typeof evaled !== "string")
+                    evaled = util.inspect(evaled);
+                    message.channel.send(clean(evaled), {code:"xl"});
+            } catch (err) {
+                message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+            }
+        }
     }
 }
