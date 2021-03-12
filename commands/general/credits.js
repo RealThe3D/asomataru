05d0a2518bc7b34bda8458f48d9e5827f282f5e9
3DNinja54 <3dninja54@gmail.com>
2021-03-11 20:47:22 -0500

    Updated to Asomataru v2.3.1.1!

--- commands/general/credits.js
+++ commands/general/credits.js
@@ -8,16 +8,16 @@ module.exports = {
 	exec: async (client, message, args) => {
 		const Discord = require('discord.js');
 		const credits = [
-			'Davska#6229 - Pro smash player, true friend, and loves the Sinnoh region!',
-			'Kandrina#1426 - Lovely woman from Austria, likes eating ribs!',
-			"Akashic Bearer#2305 - A bot programmer who totally doesn't have a better bot then me.",
-			'Mochi#7342 - Coolio person, loves to eat mochi.',
-			'Dr. Mathew#6718 aka Creation, worthy player and waifu!',
+			'Davska#6229 - Pro smash player, and loves the Sinnoh region! Happy about the announced remakes',
+			'Kandrina#1426 - Lovely woman from Austria, but plays too much Genshin!',
+			"Akashic Bearer#2305 - A discord bot programmer who totally doesn't have a better bot then me.",
+			'Mochi#7342 - Cool person, loves to eat mochi.',
+			'Dr. Mathew#6718, worthy player and waifu!',
 			'Incineroar#7934 - Fatal memer and pro smash player',
-			'Stemi#6098 - Busy with schoolwork but can manage to tell me oyasumi',
-			'SirSailor#3528 - Loves Xenoblade and Fire Emblem!',
-			"Daniel Diaz#7193 aka Monika - Luxembourg's flag looks like aquafresh.",
-			'InfiniteTime#0463 aka Asomataru-Chan, the person this bot is named after! Subscribe to their YouTube, InfiniteTimeLapse!',
+			'SteelyMite#6098 - Busy with schoolwork but can manage to tell me oyasumi',
+			'SirSailor Star#3528 - Loves Xenoblade and Fire Emblem!',
+			"Daniel Diaz#9198 - Luxembourg's flag looks like aquafresh.",
+			'InfiniteTime#0463 aka Asomataru-Chan, the person this bot is named after! Subscribe to their YouTube and Twitch',
 			'Mirjan#9930 - Fatal memer!',
 		];
 		const creditsEmbed = new Discord.MessageEmbed()
