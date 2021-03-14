2b1c886512414bf707b9c4488c48258ac267085b
3DNinja54 <3dninja54@gmail.com>
2021-03-13 21:50:20 -0500

    Updated to Asomataru v2.4!

--- commands/general/avatar.js
+++ commands/general/avatar.js
@@ -13,33 +13,22 @@ module.exports = {
 
 		if (!isNaN(args[0]) && args[0].length === 18) {
 			var member =
-				message.guild.members.cache.get(args[0]) || message.member;
+				message.guild.users.cache.get(args[0]) || message.member;
 			user = member.user;
 		} else {
 			user = message.mentions.users.first() || message.author;
 		}
-		if (member.user.bot) {
-			const botEmbed = new Discord.MessageEmbed()
-				.setTitle(`${member.user.username}'s Avatar`)
-				.setImage(
-					member.user.displayAvatarURL({
-						size: 512,
-						format: 'jpg',
-						dynamic: true,
-					})
-				);
-		} else {
-			const embed = new Discord.MessageEmbed()
-				.setTitle(`${user.username}'s Avatar`)
-				.setImage(
-					user.displayAvatarURL({
-						size: 512,
-						format: 'jpg',
-						dynamic: true,
-					})
-				);
 
-			message.channel.send(embed);
-		}
+		const embed = new Discord.MessageEmbed()
+			.setTitle(`${user.username}'s Avatar`)
+			.setImage(
+				user.displayAvatarURL({
+					size: 512,
+					format: 'jpg',
+					dynamic: true,
+				})
+			);
+
+		message.channel.send(embed);
 	},
 };
