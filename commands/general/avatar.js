b027b3cc00035146194783763c57bcc7aceda6a3
3DNinja54 <3dninja54@gmail.com>
2021-03-14 00:22:28 -0500

    Update to Asomataru v2.4!

--- commands/general/avatar.js
+++ commands/general/avatar.js
@@ -13,7 +13,8 @@ module.exports = {
 
 		if (!isNaN(args[0]) && args[0].length === 18) {
 			var member =
-				message.guild.members.cache.get(args[0]) || message.member;
+				message.guild.members.cache.get(args[0].toString()) ||
+				message.member;
 			user = member.user;
 		} else {
 			user = message.mentions.users.first() || message.author;
