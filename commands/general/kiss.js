e08c9b0ece0565e6f776276e4edc123264d4a1c8
3DNinja54 <3dninja54@gmail.com>
2021-03-13 23:55:35 -0500

    Updated to Asomataru v2.4!

--- commands/general/kiss.js
+++ commands/general/kiss.js
@@ -14,8 +14,8 @@ module.exports = {
 
 		if (!isNaN(args[0]) && args[0].length === 18) {
 			var member =
+				message.guild.members.cache.get(args[0]) ||
 				message.guild.users.cache.get(args[0]) ||
-				message.guild.members.cahche.get(args[0]) ||
 				message.member;
 			user = member.user;
 		} else {
