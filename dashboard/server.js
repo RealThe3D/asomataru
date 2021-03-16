dbcad695d7f65230ede741ccf3d16e28749d60f8
3DNinja54 <3dninja54@gmail.com>
2021-03-16 12:40:15 -0400

    last test

--- dashboard/server.js
+++ dashboard/server.js
@@ -1,10 +1,5 @@
 const express = require('express');
 const app = express();
 
-app.all('/', (req, res) => {
-	res.send('Asomataru is ready!');
-});
-
 app.use(express.static(__dirname));
-
 app.listen(3000);
