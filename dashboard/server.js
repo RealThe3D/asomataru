86b58e477a1b59a0ead54c5ccb4654dbea125af9
3DNinja54 <3dninja54@gmail.com>
2021-03-16 13:05:27 -0400

    deleteing branch

--- dashboard/server.js
+++ dashboard/server.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
-app.all('/', (req, res) => {
-	res.send("Asomataru's server is up!");
-});
 app.use(express.static(__dirname));
 app.listen(PORT);
