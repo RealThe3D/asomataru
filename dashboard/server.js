0d8a40c01ae742fff4b4eba9a9c9bc34b6ffbdb9
3DNinja54 <3dninja54@gmail.com>
2021-03-16 12:54:33 -0400

    server fix

--- dashboard/server.js
+++ dashboard/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
+app.all('/', (req, res) => {
+	res.send("Asomataru's server is up!");
+});
 app.use(express.static(__dirname));
 app.listen(PORT);
