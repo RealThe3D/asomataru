6ab4b194a8714374a078bea43c7a8a28adf89e08
3DNinja54 <3dninja54@gmail.com>
2021-03-15 22:54:09 -0400

    server ping test

--- dashboard/server.js
+++ dashboard/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-/*
+
 app.all('/', (req, res) => {
 	res.send('Asomataru is ready!');
 });
-*/
+
 app.use(express.static(__dirname));
 
 function keepAlive() {
