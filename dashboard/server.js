ca83b4ee7981d3cf652f530937f15934199ba1a2
3DNinja54 <3dninja54@gmail.com>
2021-04-14 21:34:07 -0400

    Small server-side update of v2.4.1!

--- dashboard/server.js
+++ dashboard/server.js
@@ -2,5 +2,13 @@ const express = require('express');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.use(express.static(__dirname));
-app.listen(PORT);
+app.use('/', express.static(__dirname + '/public'));
+app.use('/', express.static(__dirname + '/views'));
+
+app.get('*', (req, res) => {
+	res.sendStatus(404);
+});
+
+app.listen(PORT, () => {
+	console.log('Server is up!');
+});
