5dbf23222a2cf3613bd10e643d2731ecb6e359d6
3DNinja54 <3dninja54@gmail.com>
2021-10-12 20:40:39 -0400

    Giving asomataru her presence back.

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,8 +1,13 @@
+import { Client } from 'discord.js';
 import { Event } from '../interfaces/Event';
 
 export const event: Event = {
 	name: 'ready',
-	on: (client) => {
+	on: (client: Client) => {
 		console.log(`${client.user?.tag} is ready! | ${new Date()}`);
+		client.user?.setPresence({
+			status: 'online',
+			activities: [{ name: 'Asomataru v3 Beta', type: 'PLAYING' }],
+		});
 	},
 };
