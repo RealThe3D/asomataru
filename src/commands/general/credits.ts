706f43e02a3727a309a3190338ae78d039ed4e55
The3D <3dninja54@gmail.com>
2023-07-16 16:30:19 -0400

    Changed because of Discord's username change.

--- src/commands/general/credits.ts
+++ src/commands/general/credits.ts
@@ -3,9 +3,7 @@ import { Command } from '../../interfaces/Command';
 
 export const command: Command = {
 	name: 'credits',
-
 	ownerOnly: false,
-
 	cooldown: 0,
 	usage: 'credits',
 	data: new SlashCommandBuilder()
@@ -13,16 +11,16 @@ export const command: Command = {
 		.setDescription('Dedication towards friends and/or supporters.'),
 	execute: async (client, interaction) => {
 		const credits = [
-			'Davska#6229 - Pro smash player, and loves the Sinnoh region! Happy about the remakes!',
-			'Kandrina#1426 - Lovely woman from Austria, but plays too much Genshin!',
-			"Akashic Bearer#2305 - A discord bot programmer who totally has a better bot than me, except it's not running V13.",
-			'Mochi#7342 - Cool person, loves to eat mochi and plays a lot of Minecraft!',
-			'Dr. Mathew#6718, worthy player and awesome artist',
-			'Incineroar#7934 - Fatal memer and pro smash player',
-			'SteelyMite#6098 - Busy with schoolwork but can manage to tell me oyasumi',
-			'SirSailor Star#3528 - Loves Xenoblade and Fire Emblem!',
-			"Daniel Diaz#9198 - Luxembourg's flag looks like aquafresh.",
-			'InfiniteTime#0463 aka Asomataru-Chan, the person this bot is named after! Subscribe to their YouTube and Twitch!',
+			'Davska (davska)- Pro smash player, and loves the Sinnoh region! Happy about the remakes!',
+			'Kandrina (kandrina) - Lovely woman from Austria, but eats too many ribs',
+			"Akashic Bearer (akashicbearer) - A discord bot programmer who totally has a better bot than me, except it's not running V13.",
+			'Mochi (mo_mochi) - Cool person, loves to eat mochi and plays a lot of Minecraft!',
+			'Dr. Mathew (dr. mathew), worthy player and awesome artist',
+			'Incineroar (incine_) - Fatal memer and pro smash player',
+			'SteelyMite (steelymite) - Busy with schoolwork but can manage to tell me oyasumi',
+			'SirSailor Star (sirsailorstar) - Loves Xenoblade and Fire Emblem!',
+			'InfiniteTime (infinitetime) aka Asomataru-Chan, the person this bot is named after! Subscribe to their YouTube and Twitch!',
+			'Ditlus (ditlus) - An aspiring cook from Costa Rica! 🥰',
 		];
 		const creditsEmbed = new EmbedBuilder()
 			.setTitle('A dedication to my friends!')
