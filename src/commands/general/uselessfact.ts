a422e7175c8697be0c0f4fb21fc4b55fdbe8a67e
The3D <3dninja54@gmail.com>
2023-07-16 16:29:42 -0400

    useless fact subtle changes

--- src/commands/general/uselessfact.ts
+++ src/commands/general/uselessfact.ts
@@ -1,26 +1,25 @@
-import { SlashCommandBuilder, EmbedBuilder } from 'discord.js';
+import { SlashCommandBuilder, EmbedBuilder, Colors } from 'discord.js';
 import axios from 'axios';
 import { Command } from '../../interfaces/Command';
 
 export const command: Command = {
 	name: 'uselessfact',
-
 	ownerOnly: false,
-
 	cooldown: 3,
 	usage: 'uselessfact',
 	data: new SlashCommandBuilder()
 		.setName('uselessfact')
 		.setDescription('Sends a utterly, useless fact.'),
 	execute: async (client, interaction) => {
+		await interaction.deferReply();
 		const { data } = await axios.get(
 			'https://uselessfacts.jsph.pl/random.json?language=en'
 		);
 
 		const embed = new EmbedBuilder()
-			.setTitle('**__Useless Fact!__**')
-			.setDescription(data.text)
-			.setFooter({ text: 'Powered by uselessfacts.jsph.pl' });
+			.setColor(Colors.LuminousVividPink)
+			.setTitle('USELESS FACT')
+			.setDescription(data.text);
 
 		await interaction.reply({ embeds: [embed] });
 	},
