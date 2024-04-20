b36547263c7f4700d53453d8ba5f6e367e272027
The3D <3dninja54@gmail.com>
2024-04-20 19:12:28 -0400

    ✨ brought back the latency visibility

--- src/commands/utils/ping.ts
+++ src/commands/utils/ping.ts
@@ -1,40 +1,35 @@
-import { EmbedBuilder, SlashCommandBuilder } from 'discord.js';
+import { Colors, EmbedBuilder, SlashCommandBuilder } from 'discord.js';
 import { Command } from '../../interfaces/Command';
-import * as PACKAGE from '../../../package.json';
 
 export const command: Command = {
 	name: 'ping',
-
 	cooldown: 3,
-
 	ownerOnly: false,
 	usage: '',
 	data: new SlashCommandBuilder()
 		.setName('ping')
-		.setDescription('Replies with pong with milliseconds'),
+		.setDescription('Replies with the latency with milliseconds'),
 	execute: async (client, interaction) => {
-		const ping = new EmbedBuilder()
-			.setTitle('Ping')
-			.setDescription('Pong!')
-			.setColor(0xdff8eb)
-			.setFooter({ text: `Asomataru v${PACKAGE.version}` });
-		// await interaction.reply({ embeds: [ping] });
+		await interaction.deferReply();
 
-		// const m = await interaction.reply({ embeds: [ping] });
-		// const pong = new EmbedBuilder()
-		// 	.setTitle('Ping')
-		// 	.setDescription(
-		// 		'Pong!' +
-		// 			`Latency is ${
-		// 				 - m?.interaction.createdTimestamp!
-		// 			}ms. 🏓`
-		// 	)
-		// 	.setColor(0xdff8eb)
-		// 	.setFooter({ text: `Asomataru v${PACKAGE.version}` });
-		// pong.setDescription(
-		// 	`${Number(pong.data.timestamp) - m.interaction.createdTimestamp}`
-		// );
-		// await interaction.editReply({ embeds: [pong] });
-		await interaction.reply({ embeds: [ping] });
+		const initEmbed = new EmbedBuilder()
+			.setTitle('Pong?')
+			.setDescription('Pinging the bot....')
+			.setColor(Colors.Grey);
+
+		const newInteraction = await interaction.followUp({ embeds: [initEmbed] });
+
+		initEmbed
+			.setTitle('Pong!')
+			.setDescription(
+				`The latency is ${
+					newInteraction.createdTimestamp - interaction.createdTimestamp
+				}ms. 🏓`
+			)
+			.setColor(Colors.Green);
+
+		await newInteraction.edit({
+			embeds: [initEmbed],
+		});
 	},
 };
