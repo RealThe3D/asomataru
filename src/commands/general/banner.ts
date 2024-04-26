d715d29633cbcd4532a12d84901847ec4eb2a092
The3D <3dninja54@gmail.com>
2024-04-25 21:25:44 -0400

    ✨ Added banner.ts

---
+++ src/commands/general/banner.ts
@@ -0,0 +1,41 @@
+import { Command } from '../../interfaces/Command';
+import { EmbedBuilder, SlashCommandBuilder } from 'discord.js';
+
+export const command: Command = {
+	name: 'banner',
+	ownerOnly: false,
+	cooldown: 2,
+	usage: 'avatar (@mention or userID)',
+	data: new SlashCommandBuilder()
+		.setName('banner')
+		.setDescription("Displays a user's avatar")
+		.addUserOption((option) =>
+			option
+				.setName('user')
+				.setDescription(
+					'User banner to display. If omitted, displays your banner.'
+				)
+		),
+	execute: async (client, interaction) => {
+		const user = interaction.options.getUser('user') || interaction.user;
+		const userBanner = await user.fetch(true);
+
+		if (!userBanner.banner) {
+			return await interaction.reply({
+				content:
+					'That user does not have a banner. They need to be a Nitro member and set an image as a banner.',
+				ephemeral: true,
+			});
+		}
+		const embed = new EmbedBuilder()
+			.setTitle(`${user.username}'s Avatar`)
+			.setImage(
+				userBanner.bannerURL({
+					size: 512,
+					extension: 'png',
+				})!
+			);
+
+		await interaction.reply({ embeds: [embed] });
+	},
+};
