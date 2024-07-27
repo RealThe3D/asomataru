de2540807a916b016855905b4ee33a9ef0fa8cab
The3D <3dninja54@gmail.com>
2024-07-26 22:21:36 -0400

    change names and descriptions

--- src/commands/general/credits.ts
+++ src/commands/general/credits.ts
@@ -8,18 +8,17 @@ export const command: Command = {
 	usage: 'credits',
 	data: new SlashCommandBuilder()
 		.setName('credits')
-		.setDescription('Dedication towards friends and/or supporters.'),
+		.setDescription('A dedication towards friends and/or supporters.'),
 	execute: async (client, interaction) => {
 		const credits = [
 			'Davska (davska) - Pro smash player, and loves the Sinnoh region! Happy about the remakes!',
 			'Kandrina (kandrina) - Lovely woman from Austria, but eats too many ribs',
-			"Akashic Bearer (akashicbearer) - A discord bot programmer who totally has a better bot than me, except it's not running V13.",
-			'Mochi (mo\\_mochi) - Cool person, loves to eat mochi and plays a lot of Minecraft!',
+			'Akashic Bearer (akashicbearer) - A discord bot programmer who totally has a better bot than me, except it is not running v14.',
+			'Mochi (mo\\_mochi) - Cool person, loves to eat mochi and is not a fan of veins.',
 			'Dr. Mathew (dr.mathew) - Worthy player and awesome artist',
-			'Incineroar (incine\\_) - Fatal memer and pro smash player',
-			'SteelyMite (steelymite) - Busy with schoolwork but can manage to tell me oyasumi',
-			'SirSailor Star (sirsailorstar) - Loves Xenoblade and Fire Emblem!',
-			'InfiniteTime (infinitetime) aka Asomataru-Chan - The person this bot is named after! Subscribe to their YouTube and Twitch!',
+			'Incineroar (incinesk) - One Piece connoisseur and pro smash player!',
+			'SteelyMite (steelymite) - Graduated from University! I am proud of him!',
+			'Luna (infinitetime) aka Asomataru-Chan - The person this bot is named after! Subscribe to their YouTube and Twitch!',
 			'Ditlus (ditlus) - An aspiring cook from Costa Rica! 🥰',
 		];
 		const creditsEmbed = new EmbedBuilder()
