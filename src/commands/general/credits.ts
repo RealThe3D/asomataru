64fdaff3e3ac08e0f74aeb2fff920b4e2f1d0dd5
The3D <3dninja54@gmail.com>
2025-01-11 22:32:36 -0500

    ✨ add pagination to credits

--- src/commands/general/credits.ts
+++ src/commands/general/credits.ts
@@ -1,4 +1,13 @@
-import { Colors, EmbedBuilder, SlashCommandBuilder } from 'discord.js';
+import {
+	ActionRowBuilder,
+	ButtonBuilder,
+	ButtonStyle,
+	Colors,
+	ComponentType,
+	EmbedBuilder,
+	MessageComponentInteraction,
+	SlashCommandBuilder,
+} from 'discord.js';
 import { Command } from '@/interfaces/Command.ts';
 
 export const command: Command = {
@@ -10,24 +19,71 @@ export const command: Command = {
 		.setName('credits')
 		.setDescription('A dedication towards friends and/or supporters.'),
 	execute: async (_, interaction) => {
-		const credits = [
+		const credits = [[
+			'Luna (infinitetime) aka Asomataru-Chan - The person this bot is named after! Subscribe to their YouTube and Twitch!',
 			'Davska (davska) - Pro smash player, and loves the Sinnoh region! Happy about the remakes!',
 			'Kandrina (kandrina) - Lovely woman from Austria, but eats too many ribs',
 			'Akashic Bearer (akashicbearer) - A discord bot programmer who totally has a better bot than me, except it is not running v14.',
 			'Mochi (mo\\_mochi) - Cool person, loves to eat mochi and is not a fan of veins.',
 			'Incineroar (incinesk) - One Piece connoisseur and pro smash player!',
 			'SteelyMite (steelymite) - Graduated from University! I am proud of him!',
-			'Luna (infinitetime) aka Asomataru-Chan - The person this bot is named after! Subscribe to their YouTube and Twitch!',
 			'Ditlus (ditlus) - An aspiring cook from Costa Rica! 🥰',
 			'Winnie (winxyclub) - A local Floridian fairy, but when will they move to the eastern side?',
 			'Kayla (qtkayla13) - Pro Minecraft and volleyball player, and she also smells!',
 			'Bubba (bubbaholderofls) - Pro Hypixel player, but he should carry me through Catacombs F7!',
 			'Aidan (hirsuna) - Pro Hypixel player, and some of his quotes are the greatest humanity has ever made.',
-		];
+		], [
+			'Ethereal (ethereal_23) - Pro HSR player, happy about the release of Dragon Age: Veilguard!',
+			"Cellion (itsyahomie.) - Pro HSR player, has arguably one of the best Topaz builds I've ever seen!",
+			'SkidShot (skidshot) - Pro HSR player, has a **totally** relatable Feixiao build.',
+		]];
+
+		const buttons = new ActionRowBuilder<ButtonBuilder>().addComponents([
+			new ButtonBuilder()
+				.setCustomId('backwards')
+				.setLabel('Previous')
+				.setStyle(ButtonStyle.Secondary),
+			new ButtonBuilder()
+				.setCustomId('stop')
+				.setLabel('Stop')
+				.setStyle(ButtonStyle.Secondary),
+			new ButtonBuilder()
+				.setCustomId('forward')
+				.setLabel('Next')
+				.setStyle(ButtonStyle.Secondary),
+		]);
+
+		const filter = (i: MessageComponentInteraction) => {
+			return (
+				(i.customId == 'backwards' ||
+					i.customId == 'stop' ||
+					i.customId == 'forward') &&
+				i.user.id == interaction.user?.id
+			);
+		};
+		const collector = interaction.channel?.createMessageComponentCollector({
+			filter,
+			time: 60000,
+			componentType: ComponentType.Button,
+		});
+
+		let pageNum = 0;
+
 		const creditsEmbed = new EmbedBuilder()
 			.setTitle('A dedication to my friends!')
-			.setDescription(credits.join('\n\n'))
+			.setDescription(credits[pageNum].join('\n\n'))
 			.setColor(Colors.LuminousVividPink);
-		await interaction.reply({ embeds: [creditsEmbed] });
+
+		collector?.on('collect', async (i) => {
+			await i.deferUpdate();
+
+			switch (i.customId) {
+				case 'forward':
+					pageNum++;
+					creditsEmbed.setDescription(credits[pageNum].join('\n\n'));
+			}
+			await i.editReply({ embeds: [creditsEmbed], components: [buttons] });
+		});
+		await interaction.reply({ embeds: [creditsEmbed], components: [buttons] });
 	},
 };
